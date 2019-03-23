2d584de166cb0cacbcbfbd7ea42f79a9a4169a87
spmoolman <spmoolman@gmail.com>
2019-03-23 16:59:25 +0200

    Added space

---
+++ SolidSpace/Assets/Scroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Scroller : MonoBehaviour{
+    float scrollSpeed = -5;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 25);
+        transform.position = startPos + Vector2.up * newPos;
+	}
+}
